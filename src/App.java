27ae6c3c9088033d4c311c4c1164ab9bb2afef3f
Fabio Garcia <fabiogarciamartins2025@gmail.com>
2026-03-04 22:01:46 -0300

    versao01PIZZARIA

--- src/App.java
+++ src/App.java
@@ -1,18 +1,56 @@
 public class App {
-    
-    public static void main(String[] args) {
-        double preco =0;
-        double margem=0;
-         
-        do{
-            IO.println("Preco de custo:");
-            preco = Integer.parseInt(IO.readln());
-             IO.println("Margem de Lucro entre 10 e 50:");
-              margem= Integer.parseInt(IO.readln());
 
-        }while(preco< 0&& margem < 10 ||margem > 50);
-        
-        Produto01 meuProduto = new Produto01("Livro", preco, margem);
-        IO.print(meuProduto);
+    public static void main(String[] args) {
+
+        int opcao = -1;
+
+        do {
+            opcao = exibirMenuPrincipal();
+
+            switch (opcao) {
+                case 1 -> comprarPizza();
+                case 0 -> IO.println("Obrigado pela prefenrência!!!");
+                default -> IO.println("Opção inválida!");
+            }
+
+            pausa();
+
+        } while (opcao != 0);
+    }
+
+  
+    public static int exibirMenuPrincipal() {
+        IO.println("\n===== MENU PRINCIPAL =====");
+        IO.println("1 - Comprar pizza");
+        IO.println("0 - Sair");
+        IO.println("Escolha uma opção:");
+        return Integer.parseInt(IO.readln());
+    }
+
+    public static Pizza comprarPizza() {
+
+        Pizza p = new Pizza();
+
+        IO.println("Descrição da pizza: ");
+        p.descricao = IO.readln();   // lê String
+
+        IO.println("Quantos ingredientes deseja adicionar? ");
+        int q = Integer.parseInt(IO.readln());
+
+        if (p.podeAlterar(q)) {
+            p.adicionarIngrediente(q);
+            IO.println("\nPizza montada com sucesso!\n");
+        } else {
+            IO.println("\nQuantidade inválida! (máx: " + p.MAXingredientes + ")");
+        }
+
+        IO.println(p.toString());
+
+        return p;
+    }
+
+    public static void pausa() {
+        IO.println("\nPressione ENTER para continuar...");
+        IO.readln(); 
     }
 }
---
+++ src/Pizza.java
@@ -0,0 +1,68 @@
+
+public class Pizza {
+    // atributos
+    int qtdingredientes; 
+    int MAXingredientes = 8;
+    String descricao;
+    double precoinit = 29.0;
+    double valoringrediente = 5;
+
+    
+    public double calcularvalorFinal() {
+        if(qtdingredientes==0){
+            return precoinit; //caso nao tenha adição
+        }
+        return precoinit +  (qtdingredientes * valoringrediente);
+    }
+            //alteração de adicao?
+    public boolean podeAlterar(int quantos) {
+        return (quantos + qtdingredientes >= 0 &&
+                quantos + qtdingredientes <= MAXingredientes);
+    }
+
+    //cliente adiciona ingrediente
+    public int adicionarIngrediente(int quantos) {
+        if (podeAlterar(quantos)) {
+            qtdingredientes += quantos;
+        }
+        return qtdingredientes;
+    }
+    
+       double totalacres = qtdingredientes * valoringrediente;
+    //Emite nota
+    public String toString() {
+        return """
+       ----- NOTA DA PIZZA -----
+       Descrição: %s
+       Qtd. Ingredientes: %d
+       Total de acréscimo: R$ %.2f
+       Valor Final: R$ %.2f
+       --------------------------
+       """.formatted(descricao, qtdingredientes, totalacres, calcularvalorFinal());
+    }
+
+    
+    //   MÉTODO COMPRAR PIZZA
+    
+    public static Pizza comprarPizza() {
+
+    Pizza p = new Pizza();
+
+    IO.println("Descrição da pizza: ");
+    p.descricao = IO.readln();   
+
+    IO.println("Quantos ingredientes deseja adicionar? ");
+    int q = Integer.parseInt(IO.readln()); 
+
+    if (p.podeAlterar(q)) {
+        p.adicionarIngrediente(q);
+        IO.println("\nPizza montada com sucesso!\n");
+    } else {
+        IO.println("\nQuantidade inválida! (máx: " + p.MAXingredientes + ")");
+    }
+
+    IO.println(p.toString());
+
+    return p;
+    }
+}
